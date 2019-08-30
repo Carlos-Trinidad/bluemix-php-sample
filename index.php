15b97f4a8828a3f70bbcdf7ab74ab137814fadea
Carlos Trinidad <trinidadcarlos26@gmail.com>
2019-08-30 17:26:47 -0300

    Traduciendo al español

--- index.php
+++ index.php
@@ -12,8 +12,8 @@
 				<img class = 'newappIcon' src='images/newapp-icon.png'>
 			</td>
 			<td>
-				<h1 id = "message"><?php echo "Hello World!"; ?></h1>
-				<p class='description'></p> Thanks for creating a <span class="blue"><a href="https://github.com/IBMCloudDevOps/bluemix-php-sample">PHP Starter Application</a></span>.
+				<h1 id = "message"><?php echo "Hola Mundo!"; ?></h1>
+				<p class='description'></p> Gracias por crear una <span class="blue"><a href="https://github.com/IBMCloudDevOps/bluemix-php-sample">aplicación starter de PHP.</a></span>.
 			</td>
 		</tr>
 	</table>
